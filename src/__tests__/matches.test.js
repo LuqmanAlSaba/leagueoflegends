1227a282b6d23ed381f7d19bab5bb5d531d7d0be
Luke Al-Saba <luke@al-saba.com>
2020-08-18 11:48:50 -0400

    Update tests

--- src/__tests__/matches.test.js
+++ src/__tests__/matches.test.js
@@ -1,33 +1,63 @@
-const { getDateSuffix, getMatchResult } = require('../commands/matches');
+const {getDateSuffix, getMatchResult} = require('../commands/matches');
+
+const match1 = {
+    matchID: 1597953600000,
+    status: "CONCLUDED",
+    match_type: "BEST OF 1",
+    time: "4:00 PM",
+    team1: {
+        name: "100 Thieves",
+        abbreviatedName: "100",
+        score: "1",
+        color: "#41884d"
+    },
+    team2: {
+        name: "Cloud9",
+        abbreviatedName: "C9",
+        score: "0",
+        color: "#237cca"
+    }
+};
+
+const match2 = {
+    matchID: 1597953600000,
+    status: "CONCLUDED",
+    match_type: "BEST OF 1",
+    time: "4:00 PM",
+    team1: {
+        name: "Team Liquid",
+        abbreviatedName: "TL",
+        score: "0",
+        color: "#41884d"
+    },
+    team2: {
+        name: "TSM",
+        abbreviatedName: "TSM",
+        score: "1",
+        color: "#237cca"
+    }
+};
 
 test('returns suffix for the given date', () => {
-  expect(getDateSuffix(21)).toBe('st');
+    expect(getDateSuffix(21)).toBe('st');
 });
 
 test('returns suffix for the given date', () => {
-  expect(getDateSuffix(2)).toBe('nd');
+    expect(getDateSuffix(2)).toBe('nd');
 });
 
 test('returns suffix for the given date', () => {
-  expect(getDateSuffix(3)).toBe('rd');
+    expect(getDateSuffix(3)).toBe('rd');
 });
 
 test('returns suffix for the given date', () => {
-  expect(getDateSuffix(11)).toBe('th');
+    expect(getDateSuffix(11)).toBe('th');
 });
 
 test('returns the match result based on the match score', () => {
-  expect(getMatchResult(1, 4, 'PENDING')).toBe('Team 2');
+    expect(getMatchResult(match1)).toBe('100 Thieves');
 });
 
 test('returns the match result based on the match score', () => {
-  expect(getMatchResult(3, 2, 'PENDING')).toBe('Team 1');
-});
-
-test('returns the match result based on the match score', () => {
-  expect(getMatchResult(2, 2, 'PENDING')).toBe('');
-});
-
-test('returns the match result based on the match score', () => {
-  expect(getMatchResult(3, 2, 'IN_PROGRESS')).toBe('');
+    expect(getMatchResult(match2)).toBe('TSM');
 });
