0a87e6c60d8e5b6e88afe53f444100a5c48d0b10
Luke Al-Saba <luke@al-saba.com>
2020-09-25 19:31:35 -0400

    Add new test cases for getDate function

--- src/__tests__/matches.test.js
+++ src/__tests__/matches.test.js
@@ -1,4 +1,13 @@
-const { getDateSuffix, getMatchResult } = require('../commands/matches');
+const {
+  getDate,
+  getDateSuffix,
+  getMatchResult,
+} = require('../commands/matches');
+
+const DATE = { weekday: 'long', month: 'long', day: 'numeric' };
+const NUM_DAY = { day: 'numeric' };
+const DAY = { weekday: 'long' };
+const TIME = { hour: '2-digit', minute: '2-digit' };
 
 const match1 = {
   matchID: 1597953600000,
@@ -37,6 +46,40 @@ const match2 = {
     color: '#237cca',
   },
 };
+
+test('returns match date when it is the first match of the week', () => {
+  const currentDate = new Date(match1.matchID).toLocaleString('en-US', DATE);
+  const i = 0;
+  const matchStartDate = match1.matchID;
+  const match = {
+    status: match1.status,
+    date: new Date(matchStartDate).toLocaleString('en-US', DATE),
+    day: new Date(matchStartDate).toLocaleString('en-US', NUM_DAY),
+    weekday: new Date(matchStartDate).toLocaleString('en-US', DAY),
+    time: new Date(matchStartDate).toLocaleString('en-US', TIME),
+    team1: match1.team1,
+    team2: match1.team2,
+  };
+
+  expect(getDate(match, currentDate, i)).toBe('Thursday, August 20');
+});
+
+test('returns empty string when current date is equal to the next matches date', () => {
+  const currentDate = new Date(match1.matchID).toLocaleString('en-US', DATE);
+  const i = 1;
+  const matchStartDate = match1.matchID;
+  const match = {
+    status: match1.status,
+    date: new Date(matchStartDate).toLocaleString('en-US', DATE),
+    day: new Date(matchStartDate).toLocaleString('en-US', NUM_DAY),
+    weekday: new Date(matchStartDate).toLocaleString('en-US', DAY),
+    time: new Date(matchStartDate).toLocaleString('en-US', TIME),
+    team1: match1.team1,
+    team2: match1.team2,
+  };
+
+  expect(getDate(match, currentDate, i)).toBe('');
+});
 
 test('returns suffix for the given date', () => {
   expect(getDateSuffix(21)).toBe('st');
